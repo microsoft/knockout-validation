cc2df8b1d464adceac822ce1ac9bf2f8627ee4de
Wei Wei <lyweiwei@outlook.com>
2016-03-21 11:01:05 +0800

    add eslint to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,17 @@
 var path = require('path');
 var gulp = require('gulp');
+var eslint = require('gulp-eslint');
+var excludeGitignore = require('gulp-exclude-gitignore');
 var webpack = require('webpack-stream');
 var pkg = require('./package');
+
+gulp.task('static', function () {
+  return gulp.src('**/*.js')
+    .pipe(excludeGitignore())
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
 
 gulp.task('webpack', function () {
   return gulp.src(path.resolve(pkg.main))
@@ -10,3 +20,5 @@ gulp.task('webpack', function () {
 });
 
 gulp.task('prepublish', ['webpack']);
+
+gulp.task('default', ['static', 'webpack']);
--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,20 @@ try {
   webpackAlias = require('./webpack.alias');
 } catch (e) { }
 
+function getExternals() {
+  var deps = _.keys(pkg.peerDependencies);
+  var externals = _.object(deps, deps);
+
+  return _.reduce(_.pairs(webpackAlias), function (exts, pair) {
+    if (_.has(externals, pair[1])) {
+      exts[pair[0]] = pair[1];
+    }
+    return exts;
+  }, externals);
+}
+
 module.exports = {
-  entry: path.resolve(pkg.main),
+  entry: path.resolve('./js/index.js'),
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bingads-ko-validation.js',
@@ -18,7 +30,7 @@ module.exports = {
     umdNamedDefine: false,
     devtoolModuleFilenameTemplate: 'webpack:///bingads-ko-validation/[resource-path]',
   },
-  externals: _.keys(pkg.peerDependencies),
+  externals: [getExternals()],
   resolve: { alias: webpackAlias },
   devtool: 'source-map',
 };
